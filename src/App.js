655ea63ff5de319264896258eb55feeaf66dab85
tnix-company <help@tnix.kr>
2021-05-14 14:40:21 +0900

    LINK 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main";
+import ProductPage from "./product";
+import UploadPage from "./upload";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path={"/product/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
